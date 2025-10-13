33087b7d18f1ae49a4002352acca26a76027f88e
gmegidish <gil@megidish.net>
2025-10-13 14:07:03 +0200

    updates

---
+++ src/SidebarViewProvider.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+import { MobileCliServer } from './MobileCliServer';
+import { HtmlUtils } from './utils/HtmlUtils';
+
+export class SidebarViewProvider implements vscode.WebviewViewProvider {
+	constructor(
+		private readonly context: vscode.ExtensionContext,
+		private readonly cliServer: MobileCliServer
+	) {}
+
+	resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		token: vscode.CancellationToken
+	): void | Thenable<void> {
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [
+				vscode.Uri.joinPath(this.context.extensionUri, 'assets')
+			]
+		};
+
+		webviewView.webview.html = this.getHtml(webviewView.webview, 'sidebar');
+
+		// handle messages from the webview
+		webviewView.webview.onDidReceiveMessage(async (message) => {
+			console.log('sidebar received message:', message);
+
+			switch (message.command) {
+				case 'onInitialized':
+					console.log('sidebar webview initialized');
+					// send server port to webview
+					webviewView.webview.postMessage({
+						command: 'configure',
+						serverPort: this.cliServer.getJsonRpcServerPort(),
+					});
+					break;
+				case 'alert':
+					vscode.window.showInformationMessage(message.text);
+					break;
+			}
+		});
+	}
+
+	private getHtml(webview: vscode.Webview, page: string = 'sidebar'): string {
+		return HtmlUtils.getHtml(this.context, webview, page);
+	}
+}
---
+++ src/utils/HtmlUtils.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export class HtmlUtils {
+	/**
+	 * generates HTML content for a webview by reading index.html and replacing asset paths
+	 * @param context extension context
+	 * @param webview webview instance (panel or view)
+	 * @param page page identifier to inject as window.__VSCODE_PAGE__
+	 * @returns HTML content with replaced asset URIs and injected page identifier
+	 */
+	static getHtml(
+		context: vscode.ExtensionContext,
+		webview: vscode.Webview,
+		page: string = 'device'
+	): string {
+		const htmlPath = vscode.Uri.joinPath(context.extensionUri, 'assets', 'index.html');
+		let htmlContent = fs.readFileSync(htmlPath.fsPath, 'utf8');
+
+		const assets = ["styles.css", "bundle.js"];
+		for (const asset of assets) {
+			const uri = webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'assets', asset));
+			htmlContent = htmlContent.replace(asset, uri.toString());
+		}
+
+		// inject page query parameter into the HTML
+		const pageScript = `<script>window.__VSCODE_PAGE__ = '${page}';</script>`;
+		htmlContent = htmlContent.replace('</head>', `${pageScript}</head>`);
+
+		return htmlContent;
+	}
+}
