128e50b44cbef4e289b60727adac585326121dc8
gmegidish <gil@megidish.net>
2025-11-16 21:27:37 +0100

    allow closing getting started

--- webview-ui/src/components/GettingStartedBanner.tsx
+++ webview-ui/src/components/GettingStartedBanner.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { X } from 'lucide-react';
 import vscode from '../vscode';
 
 function GettingStartedBanner() {
+	const [isVisible, setIsVisible] = useState(true);
+
 	const handleGettingStartedClick = () => {
 		vscode.postMessage({
 			command: 'openGettingStarted'
 		});
 	};
 
+	const handleClose = () => {
+		setIsVisible(false);
+	};
+
+	if (!isVisible) {
+		return null;
+	}
+
 	return (
 		<div className="px-3 py-4">
-			<div className="mt-4 p-3 bg-[#252526] border border-[#3e3e3e] rounded-md">
-				<div className="flex flex-col gap-2">
+			<div className="mt-4 p-3 bg-[#252526] border border-[#3e3e3e] rounded-md relative">
+				<button
+					onClick={handleClose}
+					className="absolute top-2 right-2 p-1 hover:bg-[#3e3e3e] rounded transition-colors"
+					aria-label="Close banner"
+				>
+					<X className="h-4 w-4 text-[#858585] hover:text-[#cccccc]" />
+				</button>
+				<div className="flex flex-col gap-2 pr-6">
 					<p className="text-sm text-[#cccccc]">
 						Can't find the device you were looking for? No worries, here is our getting started guide.
 					</p>
